2fa90f5af83ff1c8dda8dac6bf6e4f3c5a520465
Dumitru Spataru <dimaspat12554@gmail.com>
2025-12-22 18:58:24 -0500

    making available for everyone cors of backend

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -7,49 +7,59 @@ import { createClient } from "redis";
 
 import { authRouter } from "./routes/authRouter";
 import { paymentRouter } from "./routes/paymentRouter";
-import {gameRouter} from "./routes/gameRouter";
+import { gameRouter } from "./routes/gameRouter";
 
 const app = new Hono();
 
 const redisDB = createClient({
-    username: 'default',
-    password: Bun.env.REDIS_PASSWORD || process.env.REDIS_PASSWORD,
-    socket: {
-        host: Bun.env.REDIS_HOST || process.env.REDIS_HOST,
-        port: Number(Bun.env.REDIS_PORT || process.env.REDIS_PORT),
-    }
+  username: "default",
+  password: Bun.env.REDIS_PASSWORD || process.env.REDIS_PASSWORD,
+  socket: {
+    host: Bun.env.REDIS_HOST || process.env.REDIS_HOST,
+    port: Number(Bun.env.REDIS_PORT || process.env.REDIS_PORT),
+  },
 });
 
-const start = async ():Promise<void> => {
-    await redisDB.connect().then(() => console.log("Connected Database server: Redis"));
-}
+const start = async (): Promise<void> => {
+  await redisDB
+    .connect()
+    .then(() => console.log("Connected Database server: Redis"));
+};
 
-app.use('/api/*', cors({
-    origin: [`http://localhost:${Bun.env.FRONTEND_PORT ||process.env.FRONTEND_PORT}`],
+app.use(
+  "/api/*",
+  cors({
+    origin: "*",
     credentials: true,
-}));
+  }),
+);
 
 if (Bun.env.IS_DEPLOYMENT || process.env.NODE_ENV === "development") {
-    app.use(logger());
+  app.use(logger());
 }
 
-app.route('/api/auth', authRouter);
-app.route('/api/payment', paymentRouter);
-app.route('/api/game', gameRouter);
-app.use('/api/user/*', bearerAuth({
-    verifyToken: async (token:string, c:any):Promise<boolean> => {
-        return token === getCookie(c, 'token');
-    }
-}));
+app.route("/api/auth", authRouter);
+app.route("/api/payment", paymentRouter);
+app.route("/api/game", gameRouter);
+app.use(
+  "/api/user/*",
+  bearerAuth({
+    verifyToken: async (token: string, c: any): Promise<boolean> => {
+      return token === getCookie(c, "token");
+    },
+  }),
+);
 
-app.post('/api/test', async (c:any) => {
-   const { message } = await c.req.json();
+app.get("/", async (c: any) => {
+  return c.status(200);
+});
+app.post("/api/test", async (c: any) => {
+  const { message } = await c.req.json();
 
-    return c.json({ message: 'Test endpoint hit successfully' }, 200);
+  return c.json({ message: "Test endpoint hit successfully" }, 200);
 });
 
 start().then(() => console.log("Server started successfully"));
-
 
 export default app;
 // export default {
