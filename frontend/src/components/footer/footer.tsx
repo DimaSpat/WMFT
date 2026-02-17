ac4de2629d491baf6a7b6dc45a991e2fb8370b9d
Dumitru Spataru <dimaspat12554@gmail.com>
2026-02-16 19:24:41 -0500

    foote3

--- frontend/src/components/footer/footer.tsx
+++ frontend/src/components/footer/footer.tsx
@@ -5,9 +5,6 @@ export const Footer = component$(() => {
   return (
     <footer
       style={{
-        position: "fixed",
-        bottom: 0,
-        left: 0,
         width: "100%",
         display: "flex",
         flexDirection: "column",
